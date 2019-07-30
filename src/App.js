0ef87168f03f0c87c429dfc490d17368b9e5bb74
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-07-30 19:06:09 +0500

    Add App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Control from './components/Control/Control';
+import Switcher from './components/Switcher/Switcher';
+import Cell from './components/Cell/Cell';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
+      <Control text="lenght" min={1} max={10} value={1} step={1}/>
+      <Control text="time" min={0.25} max={5} value={2} step={0.25}/>
+      <Switcher text="START"/>
+      <Cell id={1} set={1}/>
+      <Cell id={2} set={null}/>
+      <Cell id={3} set={null}/>
+      <Cell id={4} set={null}/>
+      <Cell id={5} set={null}/>
+      <Cell id={6} set={null}/>
+      <Cell id={7} set={null}/>
+      <Cell id={8} set={null}/>
+      <Cell id={9} set={null}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
