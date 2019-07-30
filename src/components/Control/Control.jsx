f4f914ae17ce88da8fe137322c39913fc1137cc5
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-07-30 21:19:51 +0500

    Add basic logic to the Control component

--- src/components/Control/Control.jsx
+++ src/components/Control/Control.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Control = (props) => {
-  const { text, min, max, value, step } = props
-  return (
-    <div>
-      Control = text - {text} . min - {min} . max - {max} . value - {value} . step - {step}
-      <button>-</button><button>+</button>
-    </div>
-  )
+class Control extends Component {
+  constructor(props) {
+    super(props)
+
+    const { text, min, max, value, step } = this.props
+    this.state = {
+      text,
+      min,
+      max,
+      value,
+      step
+    }
+
+    this.decrement = this.decrement.bind(this)
+    this.increment = this.increment.bind(this)
+  }
+
+  decrement() {
+    this.setState((state, props) => ({
+      value: (state.value > state.min ? state.value - state.step : state.value)
+    }), () => {
+      this.props.changeValue(this.state.value)
+    })
+  }
+
+  increment() {
+    this.setState((state, props) => ({
+      value: (state.value < state.max ? state.value + state.step : state.value)
+    }), () => {
+      this.props.changeValue(this.state.value)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Control = text - {this.state.text}
+        . min - {this.state.min}
+        . max - {this.state.max}
+        . value - {this.state.value}
+        . step - {this.state.step}
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Control
