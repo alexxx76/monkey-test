0fa1a0fa11165f91ad5e02557b6f22ca7beff800
Alexander Karpenko <alexxx76gamer@gmail.com>
2019-07-31 18:28:22 +0500

    Add reaction on change props status in the Cell component

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 
 const Cell = (props) => {
-  const { id, set } = props
+  const { id, data } = props
+  const { transmit } = props
+
+  let inlinestyle = {}
+  switch (data.status) {
+    case 'clear':
+      inlinestyle = {background: ''}
+      break
+    case 'show':
+      inlinestyle = {background: '#acc'}
+      break
+    case 'hide':
+      inlinestyle = {background: '#cac'}
+      break
+    case 'success':
+      inlinestyle = {background: '#cfc'}
+      break
+    case 'fail':
+      inlinestyle = {background: '#fcc'}
+      break
+    default:
+      inlinestyle = {background: ''}
+  }
+
+  const handleClick = () => {
+    transmit(id, data.value)
+  }
+
   return (
     <div>
-      Cell = id - {id} {set ? `. set - ${set}` : null}
-      <button>click</button>
+      Cell = id - {id}
+      . - {data.value ? data.value : null}
+      <button onClick={handleClick} style={inlinestyle}>click</button>
     </div>
   )
 }
